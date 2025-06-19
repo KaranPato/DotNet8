4052b207376127eb5815b9f19e54525773137b26
Karan Patokar <karan.123patokar@gmail.com>
2025-06-19 13:02:09 +0530

    Added another temprature

--- DotNet8_WebAPI/DotNet8_WebAPI/Controllers/WeatherForecastController.cs
+++ DotNet8_WebAPI/DotNet8_WebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DotNet8_WebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Cold"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
